333405d01107fc914824fbd6fb1d806e10634d1b
vihaan08 <70809815+vihaan08@users.noreply.github.com>
2020-10-17 11:46:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -47,17 +47,42 @@ function renderPatty() {
 
 function renderCheese() {
   //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let cheese= document.querySelector("#cheese")
+  if(state.Cheese)
+  cheese.style.display="inherit"
+
+else{
+  cheese.style.display="none"
 }
 
+}
 function renderTomatoes() {
   //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
-}
 
+let tomatoes= document.querySelector("#tomato")
+  if(state.Tomatoes)
+  tomatoes.style.display="inherit"
+
+else{
+  tomatoes.style.display="none"}
+}
 function renderOnions() {
+  let onions= document.querySelector("#onion")
+  if(state.Onions)
+  onions.style.display="inherit"
+
+else{
+  onions.style.display="none"}
   //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
 }
 
 function renderLettuce() {
+  let lettuce= document.querySelector("#lettuce")
+  if(state.Lettuce)
+  lettuce.style.display="inherit"
+
+else{
+  lettuce.style.display="none"}
   //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
 }
 
@@ -70,16 +95,27 @@ document.querySelector(".btn-patty").onclick = function () {
 
 
 // Trial 2 - Setup event listener for the tomatoes button
-
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the onion button
-
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the lettuce button
-
-
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();
+};
 //Challenge 1 - Add/Remove the class active to the buttons based on state
-
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+};
 
 //Challenge 2 - Render only the items selected in the ingredients board based on the state
 
